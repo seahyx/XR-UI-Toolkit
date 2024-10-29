cba1563b54fd62935819c632a4de57951735a263
Seah Ying Xiang <seahyx@gmail.com>
2024-10-29 20:24:47 +0800

    Fixed broken linkage from refactoring

--- Packages/com.seahyx.xruitoolkit/Editor/Core/AssetPaths.cs
+++ Packages/com.seahyx.xruitoolkit/Editor/Core/AssetPaths.cs
@@ -3,6 +3,6 @@ namespace XRUIToolkit.Core {
 	public static class AssetPaths
 	{
 		public static string packageRootDir => "Packages/com.seahyx.xruitoolkit/";
-		public static string stylesheetDir => packageRootDir + "Core/Assets/InspectorStyles.uss";
+		public static string stylesheetDir => packageRootDir + "Runtime/Core/Assets/InspectorStyles.uss";
 	}
 }
